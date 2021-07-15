36e11a958915b14b31e6c8d0ab69d18c442db8c0
Abhijith Dameruppala <74647983+abh1jith@users.noreply.github.com>
2021-07-15 22:47:38 +0530

    Update tic_tac_toe-basic.py

--- tic_tac_toe-basic.py
+++ tic_tac_toe-basic.py
@@ -3,104 +3,84 @@
 
 
 
+# Code by Abhijith
 
-
-# Printing game progress
-#from IPython.display import clear_output
-def display(x,y,z):
-#    clear_output()
-    print(f"      {x[0]} | {x[1]} | {x[2]}")
-    print("     ------------")
-    print(f"      {y[0]} | {y[1]} | {y[2]}")
-    print("     ------------")
-    print(f"      {z[0]} | {z[1]} | {z[2]}")
-
-# Printing Instructions
-def display2(x,y,z):
-    print(f"row 0 ->      {x[0]} | {x[1]} | {x[2]}")
-    print("             ------------")
-    print(f"row 1 ->      {y[0]} | {y[1]} | {y[2]}")
-    print("             ------------")
-    print(f"row 2 ->      {z[0]} | {z[1]} | {z[2]}")
-    print("              ^   ^    ^")
-    print("              |   |    |")
-    print("            col0 col1 col2")
-    print('\n')
-
-
-# Winning the game
-def end_game(r0, r1, r2):
-    if(r0 == ['X','X','X'] or r1 == ['X','X','X'] or r2 == ['X','X','X']):
-        print("!!! User-2 won the game !!!")
-    elif(r0 == ['O','O','O'] or r1 == ['O','O','O'] or r2 == ['O','O','O']):
-        print("!!! User-1 won the game !!!")
-    elif((r0[0] == 'X' and r1[0] == 'X' and r2[0] == 'X') or (r0[1] == 'X' and r1[1] == 'X' and r2[1] == 'X') or (r0[2] == 'X' and r1[2] == 'X' and r2[2] == 'X')):
-        print("!!! User-2 won the game !!!")
-    elif((r0[0] == 'O' and r1[0] == 'O' and r2[0] == 'O') or (r0[1] == 'O' and r1[1] == 'O' and r2[1] == 'O') or (r0[2] == 'O' and r1[2] == 'O' and r2[2] == 'O')):
-        print("!!! User-1 won the game !!!")
-    elif((r0[0] == 'X' and r1[1] == 'X' and r2[2] == 'X') or (r0[2] == 'X' and r1[1] == 'X' and r2[0] == 'X')):
-        print("!!! User-2 won the game !!!")
-    elif((r0[0] == 'O' and r1[1] == 'O' and r2[2] == 'O') or (r0[2] == 'O' and r1[1] == 'O' and r2[0] == 'O')):
-        print("!!! User-1 won the game !!!")
+def notempty(r1, r2, r3):
+    r = r1 + r2 + r3
+    if(" " not in r):
+        return False
     else:
-        pass
+        return True
 
-# Taking input
-def user_input():
-    r = int(input("Please enter the row number: "))
-    c = int(input("Please enter the columnn number: "))
-    if r in range(0,3) and c in range(0,3):
-        return r,c
-    else:
-        print("Please enter the numbers in range!")
-        user_input()
+def insert(r, c, k):
+    if(r == 0):
+        if(r1[c] == " "):
+            r1[c] = k
+        else:
+            print("Invalid")
+            return
+    elif(r == 1):
+        if(r2[c] == " "):
+            r2[c] = k
+        else:
+            print("Invalid")
+            return
+    else:# Code by Abhijith
+        if(r3[c] == " "):
+            r3[c] = k
+        else:
+            print("Invalid")
+            return
+# Code by Abhijith
+def game(r1, r2, r3):
+    # Row
+    if(r1[0] == r1[1] == r1[2] != " "):
+        print(f"{r1[0]} won")
+        return True
+    elif(r2[0] == r2[1] == r2[2] != " "):
+        print(f"{r1[0]} won")
+        return True
+    elif(r3[0] == r3[1] == r3[2] != " "):
+        print(f"{r1[0]} won")
+        return True
 
-# Editing rows for X (Even iterations)
-def edit_row_even(r,c):
-    if r == 0:
-        row0[c] = 'X'
-    elif r == 1:
-        row1[c] = 'X'
-    else:
-        row2[c] = 'X'
-    display(row0,row1,row2)
+    # Column
+    for i in range(3):
+        if(r1[i] == r2[i] == r3[i] != " "):
+            print(f"{r1[i]} won")
+            return True
+    
+    # Diagonals
+    if(r1[0] == r2[1] == r3[2] != " "):
+        print(f"{r1[0]} won")
+        return True
+    elif(r1[2] == r2[1] == r3[0] != " "):
+        print(f"{r1[2]} won")
+        return True
 
-# Editing rows for O (Odd iterations)
-def edit_row_odd(r,c):
-    if r == 0:
-        row0[c] = 'O'
-    elif r == 1:
-        row1[c] = 'O'
+# Code by Abhijith
+count = 1
+r1 = [" ", " ", " "]
+r2 = [" ", " ", " "]
+r3 = [" ", " ", " "]
+print(r1)
+print(r2)
+print(r3)
+while(notempty(r1, r2, r3)):
+    r = int(input("Enter row number: "))
+    c = int(input("Enter column number: "))# Code by Abhijith
+    if(count % 2 == 0):
+        insert(r, c, 'O')
     else:
-        row2[c] = 'O'
-    display(row0,row1,row2)
+        insert(r, c, 'X')
+    
+    print(r1)
+    print(r2)
+    print(r3)
+    count = count + 1
+    if(game(r1, r2, r3)):
+        break# Code by Abhijith
 
-#Running of iterations
-def run_game(i):
-    if i % 2 == 0:
-        edit_row_even(r,c)
-    else:
-        edit_row_odd(r,c)
-
-# Actual game progress
-print("Welcome! This is the format of the game:")
-row0 = [' ',' ',' ']
-row1 = [' ',' ',' ']
-row2 = [' ',' ',' ']
-display(row0,row1,row2)
-print("Do remember that the first person (User-1) and the second person (User-2) will have to choose positions for 'O' and 'X' respectively.")
-print("\nThe input has to be given with respect to the positions as shown")
-#display(['row = 0, column = 0','row = 0, column = 1','row = 0, column = 2'],['row = 1, column = 0','row = 1, column = 1','row = 1, column = 2'],['row = 2, column = 0','row = 2, column = 1','row = 2, column = 2'])
-display2(row0,row1,row2)
-i = 0
-play = True
-while(play == True):
-    end_game(row0,row1,row2)
-    p = input("Enter 'Y' if you still want to continue: ")
-    if(p == 'y' or p == 'Y'):
-        i += 1
-        r,c = user_input()
-        run_game(i)
-    else:
-        print("Thankyou for trying! \n-Abhijith")
-        play = False
+if(notempty(r1, r2, r3) == False):
+    print("DRAW")
+# Code by Abhijith
